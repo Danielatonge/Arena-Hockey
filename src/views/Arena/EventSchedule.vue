1781333170ddd50cea4d59f75ad317f7097b481f
elamboatonge <elamboatonge@gmail.com>
2021-09-10 15:03:54 +0300

    shows event on calendar

--- src/views/Arena/EventSchedule.vue
+++ src/views/Arena/EventSchedule.vue
@@ -10,17 +10,33 @@
         </v-tabs>
       </v-row>
       <v-sheet tile height="74" class="d-flex align-center" color="#EBF5FB">
-        <div style="width: 200px">
-          <v-select
-            v-model="currentDay"
-            dense
-            single-line
-            outlined
-            prepend-inner-icon="mdi-calendar-blank"
-            hide-details
-            class="ma-2 white d-flex"
-            label="Дата"
-          ></v-select>
+        <div style="width: 200px" class="ml-3">
+          <v-menu
+            v-model="date_picker"
+            :close-on-content-click="false"
+            :nudge-right="40"
+            transition="scale-transition"
+            offset-y
+            min-width="auto"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-text-field
+                v-model="date"
+                prepend-inner-icon="mdi-calendar"
+                readonly
+                single-line
+                outlined
+                dense
+                hide-details
+                v-bind="attrs"
+                v-on="on"
+              ></v-text-field>
+            </template>
+            <v-date-picker
+              v-model="date"
+              @input="date_picker = false"
+            ></v-date-picker>
+          </v-menu>
         </div>
         <div style="width: 200px">
           <v-select
@@ -63,11 +79,11 @@
         <!--          <v-icon>mdi-chevron-right</v-icon>-->
         <!--        </v-btn>-->
       </v-sheet>
-      <v-sheet  height="600" class="px-4 pb-4 overflow-auto">
+      <v-sheet height="600" class="px-4 pb-4 overflow-auto">
         <v-calendar
           v-show="value !== 3"
           ref="calendar"
-          v-model="value"
+          v-model="date"
           :weekdays="weekday"
           :weekday-format="myDayFormat"
           :type="type"
@@ -76,28 +92,61 @@
           :event-overlap-threshold="30"
           :event-color="getEventColor"
           :interval-format="intervalFormat"
-          @change="getEvents"
+          @click:event="showEvent"
+          @click:more="viewDay"
+          @click:date="viewDay"
         ></v-calendar>
-        <div v-show="value === 3 && arena_events.length === 0" class="text-center">
-          <h3 class="grey--text lighten-3--text text-h6 mb-2">Нет мероприятий</h3>
+        <v-menu
+          v-model="selectedOpen"
+          :close-on-content-click="false"
+          :activator="selectedElement"
+          offset-x
+        >
+          <v-card color="grey lighten-4" min-width="350px" flat>
+            <v-toolbar :color="selectedEvent.color" dark>
+              <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
+              <v-spacer></v-spacer>
+            </v-toolbar>
+            <v-card-text>
+              <span v-html="selectedEvent.description"></span>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn text color="secondary" @click="selectedOpen = false">
+                Cancel
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-menu>
+        <div
+          v-show="value === 3 && arena_events.length === 0"
+          class="text-center"
+        >
+          <h3 class="grey--text lighten-3--text text-h6 mb-2">
+            Нет мероприятий
+          </h3>
         </div>
-        <div v-show="value === 3" v-for="(event,index) in arena_events" :key="index"
-          class="mb-10 pl-5">
+        <div
+          v-show="value === 3"
+          v-for="(event, index) in arena_events"
+          :key="index"
+          class="mb-10 pl-5"
+        >
           <div class="grey--text lighten-3--text text-h6 mb-2">
-            {{event.date}}
+            {{ event.date }}
           </div>
-          <div class="text-h6 mb-2 font-weight-bold">{{event.title}} </div>
+          <div class="text-h6 mb-2 font-weight-bold">{{ event.title }}</div>
           <div class="body-1 blue--text mb-2">
-            {{event.type}}
+            {{ event.type }}
           </div>
           <div class="grey--text">
-            {{event.startTime}}
+            {{ event.startTime }}
           </div>
           <div class="body-1 blue--text mb-2 pt-4">
-            {{event.phone}}
+            {{ event.phone }}
           </div>
         </div>
       </v-sheet>
+      {{ focus }}
     </div>
   </div>
 </template>
@@ -124,6 +173,20 @@ export default {
   },
   computed: {
     ...mapState(["current_arena", "arena_events"]),
+    events() {
+      return this.arena_events.map((event) => ({
+        name: event.title,
+        start: new Date(
+          `${event.date}T${event.startTime ? event.startTime : "00:00"}:00`
+        ),
+        end: new Date(
+          `${event.date}T${event.endTime ? event.endTime : "23:59"}:59`
+        ),
+        color: this.colors[this.rnd(0, this.colors.length - 1)],
+        timed: true,
+        description: event.description,
+      }));
+    },
   },
   created() {
     const arenaId = this.$route.params.id;
@@ -133,13 +196,19 @@ export default {
   data() {
     return {
       type: "week",
+      focus: "",
       schedule_nav: ["Дневное", "Недельное", "Месячное", "Список мероприятий"],
-      currentDay: "23.09.2021, пн",
       mode: "column",
       mode_lesson: ["0", "1", "2"],
       weekday: [0, 1, 2, 3, 4, 5, 6],
       value: 1,
-      events: [],
+      selectedEvent: {},
+      selectedElement: null,
+      selectedOpen: false,
+      date: new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
+        .toISOString()
+        .substr(0, 10),
+      date_picker: false,
       colors: [
         "blue",
         "indigo",
@@ -149,48 +218,17 @@ export default {
         "orange",
         "grey lighten-1",
       ],
-      names: [
-        "Meeting",
-        "Holiday",
-        "PTO",
-        "Travel",
-        "Event",
-        "Birthday",
-        "Conference",
-        "Party",
-      ],
-      arenaId: ''
+      arenaId: "",
     };
   },
   methods: {
     intervalFormat(interval) {
       return interval.time;
     },
-    getEvents({ start, end }) {
-      const events = [];
-
-      const min = new Date(`${start.date}T00:00:00`);
-      const max = new Date(`${end.date}T23:59:59`);
-      const days = (max.getTime() - min.getTime()) / 86400000;
-      const eventCount = this.rnd(days, days + 20);
-
-      for (let i = 0; i < eventCount; i++) {
-        const allDay = this.rnd(0, 3) === 0;
-        const firstTimestamp = this.rnd(min.getTime(), max.getTime());
-        const first = new Date(firstTimestamp - (firstTimestamp % 900000));
-        const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000;
-        const second = new Date(first.getTime() + secondTimestamp);
-
-        events.push({
-          name: this.names[this.rnd(0, this.names.length - 1)],
-          start: first,
-          end: second,
-          color: this.colors[this.rnd(0, this.colors.length - 1)],
-          timed: !allDay,
-        });
-      }
-
-      this.events = events;
+    viewDay({ date }) {
+      this.value = 0;
+      this.date = date;
+      this.type = "day";
     },
     myDayFormat(day) {
       switch (day.weekday) {
@@ -212,6 +250,24 @@ export default {
     },
     getEventColor(event) {
       return event.color;
+    },
+    showEvent({ nativeEvent, event }) {
+      const open = () => {
+        this.selectedEvent = event;
+        this.selectedElement = nativeEvent.target;
+        requestAnimationFrame(() =>
+          requestAnimationFrame(() => (this.selectedOpen = true))
+        );
+      };
+
+      if (this.selectedOpen) {
+        this.selectedOpen = false;
+        requestAnimationFrame(() => requestAnimationFrame(() => open()));
+      } else {
+        open();
+      }
+
+      nativeEvent.stopPropagation();
     },
     rnd(a, b) {
       return Math.floor((b - a + 1) * Math.random()) + a;
