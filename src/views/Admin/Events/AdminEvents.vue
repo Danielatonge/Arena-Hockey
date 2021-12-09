427c664b60e4c499d1e636e892153d2551faec1a
Ekaterina Gibova <katyagibova@bk.ru>
2021-12-10 01:09:50 +0300

    event tables are displayed correctly

--- src/views/Admin/Events/AdminEvents.vue
+++ src/views/Admin/Events/AdminEvents.vue
@@ -1,7 +1,9 @@
 <template>
     <div>
       <div class="text-h4 mb-6">Расписание мероприятий</div>
-        <v-row>
+      <!-- Мой вариант -->
+
+        <!-- <v-row>
           <v-col>
             <v-select
             :items="allArenas"
@@ -15,18 +17,41 @@
             hide-details="auto"
             ></v-select>
           </v-col>
-        </v-row>
-      <div  class="rounded-lg white">
+        </v-row> -->
 
-      <admin-schedule-event :arenaId="arenaId" :userId="userId" />
+        <!-- Вариант Влада -->
+
+        <v-tabs
+        v-model="tab"
+        background-color="#0681c8"
+        corol="primary"
+        color = "white"
+        centered
+        icons-and-text
+        >
+          <v-tab
+          v-for="arena in allArenas"
+          :key="arena.arenaId"
+          @change="showArenaId(arena.arenaId)"
+          >
+            {{ arena.arenaTitle }}
+          </v-tab>
+        </v-tabs>
+        
+        <v-tabs-items v-model="tab">
+          <v-tab-item 
+          v-for="arena in allArenas"
+          :key="arena.arenaId">
+            <admin-schedule-event :arenaId="componentKey" :userId="userId" :key="componentKey" />
+          </v-tab-item>
+        </v-tabs-items>
+    <div  class="rounded-lg white">
 
     </div>
   </div>
 </template>
 
 <script>
-import { mapState } from "vuex";
-import moment from "moment";
 import Axios from "axios";
 import { GET_ARENAS } from "@/api";
 import AdminScheduleEvent from '../Arena/View/AdminScheduleEvent.vue';
@@ -42,170 +67,46 @@ export default {
     },
   },
   watch: {
-    value(x) {
-      if (x === 0) this.type = "day";
-      if (x === 1) this.type = "week";
-      if (x === 2) this.type = "month";
-    },
-  },
-  filters: {
-    descriptionLength(value) {
-      if (!value) return "";
-      if (value.length < 30) {
-        return value;
-      }
-      return value.slice(0, 30) + "...";
-    },
   },
   computed: {
-    ...mapState("arena", ["events"]),
-    ievents() {
-      const events = [];
-      this.events.forEach((event) => {
-        const nEvents = this.generateEvents(event);
-        events.push(...nEvents);
-      });
-      return events;
-    },
   },
   created() {
-    this.$store.dispatch("arena/getEvents", this.arenaId);
     this.getArenas()
   },
   data() {
     return {
+      tab: null,
       arenaId: '',
-      type: "week",
-      focus: "",
-      schedule_nav: ["Дневное", "Недельное", "Месячное", "Список мероприятий"],
-      mode: "column",
-      mode_lesson: ["0", "1", "2"],
-      weekday: [0, 1, 2, 3, 4, 5, 6],
-      value: 1,
-      selectedEvent: {},
-      selectedElement: null,
-      selectedOpen: false,
-      date: new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
-        .toISOString()
-        .substr(0, 10),
-      date_picker: false,
-      colors: [
-        "blue",
-        "indigo",
-        "deep-purple",
-        "cyan",
-        "green",
-        "orange",
-        "grey lighten-1",
-      ],
       allArenas: [],
+      componentKey: 0,
     };
   },
   methods: {
-    intervalFormat(interval) {
-      return interval.time;
-    },
-    viewDay({ date }) {
-      this.value = 0;
-      this.date = date;
-      this.type = "day";
-    },
-    getEventColor(event) {
-      return event.color;
-    },
-    showEvent({ nativeEvent, event }) {
-      const open = () => {
-        this.selectedEvent = event;
-        this.selectedElement = nativeEvent.target;
-        requestAnimationFrame(() =>
-          requestAnimationFrame(() => (this.selectedOpen = true))
-        );
-      };
-
-      if (this.selectedOpen) {
-        this.selectedOpen = false;
-        requestAnimationFrame(() => requestAnimationFrame(() => open()));
-      } else {
-        open();
-      }
-      nativeEvent.stopPropagation();
-    },
-    rnd(a, b) {
-      return Math.floor((b - a + 1) * Math.random()) + a;
-    },
-    generateEvents(event) {
-      const selectedDays = event.days;
-      const startDate = event.startDate,
-        endDate = event.endDate,
-        startTime = event.startTime,
-        endTime = event.endTime,
-        title = event.title,
-        description = event.description,
-        repeat = event.repeat;
-      const color = this.colors[this.rnd(0, this.colors.length - 1)];
-
-      const startMoment = moment(
-        `${startDate}T${startTime ? startTime : "00:00"}:00`
-      );
-      const endMoment = moment(`${endDate}T${endTime ? endTime : "00:00"}:00`);
-      const arr_events = [];
-      for (let i = 0; i < 7; i++) {
-        for (let j = 0; j < selectedDays.length; j++) {
-          if (Number(startMoment.day()) === selectedDays[j]) {
-            const startMomentDate = startMoment.format("YYYY-MM-DDTHH:mm:ss");
-            const beginMoment = moment(startMomentDate);
-
-            while (
-              Math.ceil(endMoment.diff(beginMoment, "weeks", true)) !== 0
-            ) {
-              const stopInterval = beginMoment.format("YYYY-MM-DD");
-              const stopMoment = moment(
-                `${stopInterval}T${endTime ? endTime : "00:00"}:00`
-              );
-              arr_events.push({
-                name: title,
-                start: beginMoment.format("YYYY-MM-DDTHH:mm:ss"),
-                end: stopMoment.format("YYYY-MM-DDTHH:mm:ss"),
-                color: color,
-                timed: true,
-                description: description,
-              });
-              if (repeat === 0) break;
-              beginMoment.add(1, "weeks");
-            }
-
-            break;
-          }
-        }
-        startMoment.add(1, "days");
-      }
-      return arr_events;
-    },
-
     async getArenas() {
         console.log(this.userId)
         let id = this.userId
       await Axios.get(`${GET_ARENAS}${id}/arenas/titles`).then( (res) => {
           res.data.forEach(elem => {
             let newObject = {
-                arenaId: elem.arenaId,
-                arenaTitle: elem.arenaTitle,
+              arenaId: elem.arenaId,
+              arenaTitle: elem.arenaTitle,
             }
             this.allArenas.push(newObject)
-            console.log(this.allArenas)
+            
           })
-        console.log(res.data)
+          console.log(this.allArenas)
       })
       .catch((error) => {
         console.error(error);
       })
     },
+
+    showArenaId(id){
+      this.componentKey = id
+    }
   },
 };
 </script>
 
 <style>
-.vertical-scroll {
-  overflow: auto;
-}
 </style>
